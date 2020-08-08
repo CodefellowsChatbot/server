ae247abd09c7f665a5a9cc3e9d1677a5a374c3d4
Lee-Roy King <leeroywking@gmail.com>
2020-08-08 10:17:14 -0700

    proof of life endpoint should work

--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # coding: utf-8
-from flask import Flask
+from flask import Flask, request, jsonify
 import os, sys
 
 currentdir = os.path.dirname(os.path.realpath(__file__))
@@ -24,6 +24,17 @@ def entry_point():
 @app.route("/question")
 def question_route():
     return "I don't have an answer for that question yet"
+
+
+@app.route("/proof_of_life", methods=["GET", "POST"])
+def handle_proof_of_life():
+    try:
+        content = request.json
+        text = content["text"]
+        print(f"Incoming request with string: {text}")
+        return jsonify({"text": text, "success": True})
+    except:
+        return jsonify({"text": "Sorry That didn't work", "success": False})
 
 
 if __name__ == "__main__":
