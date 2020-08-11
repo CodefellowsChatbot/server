b07c7fd23d7167e13fc77d4d4adfd4eca78da5e3
vorSherer <tcpma2011@gmail.com>
2020-08-10 19:37:40 -0700

    Began web scraping for course calendar data.

---
+++ webscraping/thomas/tomz_scraper.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.codefellows.org/"
+
+def drill_down(URL):
+    response = requests.get(URL)
+    # print(response)
+
+
+
+
+
+if __name__ == "__main__":
+    URL = "https://www.codefellows.org/"
+    drill_down(URL)
