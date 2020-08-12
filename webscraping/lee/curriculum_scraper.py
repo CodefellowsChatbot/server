c651a677d4798d872841f0eb3b5555aeeffe95c5
Lee-Roy King <leeroywking@gmail.com>
2020-08-11 20:14:03 -0700

    courses are complete for now

--- webscraping/lee/curriculum_scraper.py
+++ webscraping/lee/curriculum_scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 from resources.links import Links
 links = Links()
 
+
 def get_tuition_and_duration(page) -> dict:
     big_soup = BeautifulSoup(page.content, "html.parser")
     side_bar = big_soup.find_all("div", class_="sidebar_bg")
@@ -25,29 +26,45 @@ def get_tuition_and_duration(page) -> dict:
             pass
     return output
 
-def get_overview(page):
+def get_articles(page):
     big_soup = BeautifulSoup(page.content, "html.parser")
     articles = big_soup.find_all("article")
-    print(len(articles))
+    # print(len(articles))
     articles_dict = {}
     for art in articles:
         for entry in art.find_all("div"):
-            title = entry.find("h2").text.strip()
-            body = entry.text.strip()
-            articles_dict[title] = body
-    print(articles_dict.keys())
+            try:
+                title = entry.find("h2").text.strip()
+                body = entry.text.strip()[len(title) +1:]
+                articles_dict[title] = body
+            except AttributeError:
+                pass
+    return articles_dict
 
-page = requests.get(links.courses["javascript"][301])
-get_overview(page)
+# page = requests.get(links.courses["javascript"][301])
+# get_overview(page)
+def get_course_info():
+    course_obj = {}
+    for course in links.courses:
+        for level in links.courses[course]:
+            try:
+                page = requests.get(links.courses[course][level])
+                tuition_and_duration = get_tuition_and_duration(page)
+                articles = get_articles(page)
+                tuition_and_duration.update(articles)
+                output = tuition_and_duration
 
-# for course in links.courses:
-#     for level in links.courses[course]:
-#         try:
-#             page = requests.get(links.courses[course][level])
-#             output = get_tuition_and_duration(page)
-#             print(f"info for {course} {level}")
-#             print(output)
-#         except IndexError:
-#             print(f"error with {course}:{level}")
+                # print(f"info for {course} {level}")
+                try:
+                    course_obj[course]
+                except KeyError:
+                    course_obj[course] = {}
+                course_obj[course][level] = output
+                # print(output)
+            except IndexError:
+                print(f"error with {course}:{level}")
+    return course_obj
 
-# for language in links
+if __name__ == "__main__":
+    output = get_course_info()
+    print(output)
