2722a81f8e17a298d620e75892029e25cc0b1d6f
Roman Sydoruk <sydoruk89@gmail.com>
2020-08-11 22:25:31 -0700

    how to apply data

--- webscraping/roman/prospective.py
+++ webscraping/roman/prospective.py
@@ -23,4 +23,23 @@ def get_event_calendar(URL):
         event_calendar.append(course)
     return event_calendar
 
-print(get_event_calendar('https://www.codefellows.org/events-calendar/'))
+def how_to_apply(URL):
+    data = {'how to apply': {}}
+    response = requests.get(URL)
+    content = response.content
+    soup = BeautifulSoup(content, 'html.parser')
+    prep = soup.find(id='preparation')
+    step_1 = soup.find(id='step-1')
+    step_2 = soup.find(id='step-2')
+    step_3 = soup.find(id='step-3')
+    step_4 = soup.find(id='step-4')
+    reserve = soup.find(id='reserve')
+    data['how to apply'][f'{prep.get_text()}'] = prep.find_next_sibling().get_text()
+    data['how to apply'][f'{step_1.get_text()}'] = step_1.find_next_sibling().get_text()
+    data['how to apply'][f'{step_2.get_text()}'] = step_2.find_next_sibling().get_text()
+    data['how to apply'][f'{step_3.get_text()}'] = step_3.find_next_sibling().get_text()
+    data['how to apply'][f'{step_4.get_text()}'] = step_4.find_next_sibling().get_text()
+    data['how to apply'][f'{reserve.get_text()}'] = [reserve.find_all_next()[i].get_text() for i in range(8)]
+    return data
+print(how_to_apply('https://www.codefellows.org/how-to-apply/'))
+    
