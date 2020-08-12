b673301ae3ced13898a101b9a0f50bb476fe0c72
vorSherer <tcpma2011@gmail.com>
2020-08-11 18:30:38 -0700

    Second attempt code.

--- webscraping/thomas/tomz_scraper.py
+++ webscraping/thomas/tomz_scraper.py
@@ -1,34 +1,60 @@
-import json
+# import json
 import requests
+# next try:
+from collections import OrderedDict
 
 URL = "https://s3-us-west-2.amazonaws.com/static.codefellows.org/courses/schedule.json"
 
-# def data_harvest(URL):
-#     response = requests.get(URL)
+def data_harvest(URL):
+    response = requests.get(URL)
     # print(response.json()["courses"][0]["course"]["title"])
 
 # replicate the above function, but that will access all of the courses, not just courses[0]
 
-def cf_calendar(URL):
-    response = requests.get(URL)
-    print("Status code ", response.status_code)
-    print(response.json()["courses"])
+# def cf_calendar(URL):
+#     response = requests.get(URL)
+#     # print("Raise status ", response.raise_for_status())
+#     print("Status code ", response.status_code)
+#     print(response.json()["courses"])
+    # with open("response", "r") as read_file:
+    #     data = json.load(read_file)
 
-    for i in response:
-        # fields to capture:
-        #     code, 
-        print(response.json()["courses"][i]["course"]["code"])
+    # for i in data:
+    #     # fields to capture:
+    #     #     code, 
+    #     print(data["courses"][i]["course"]["code"])
+    #     #     title, 
+    #     print(data[courses][i]["course"]["title"])
+    #     #     startDate, 
+    #     print(data[courses][i]["course"]["startDate"])
+    #     #     track, 
+    #     print(data[courses][i]["course"]["track"])
+    #     #     price, 
+    #     print(data[courses][i]["course"]["price"])
+    #     #     sequenceAndFamily.
+    #     print(data[courses][i]["course"]["sequenceAndFamily"])
+
+    # for i in response:
+    #     # fields to capture:
+    #     #     code, 
+    #     print(response.json()["courses"][i]["course"]["code"])
         #     title, 
-        print(response.json()[courses][i]["course"]["title"])
-        #     startDate, 
-        print(response.json()[courses][i]["course"]["startDate"])
-        #     track, 
-        print(response.json()[courses][i]["course"]["track"])
-        #     price, 
-        print(response.json()[courses][i]["course"]["price"])
-        #     sequenceAndFamily.
-        print(response.json()[courses][i]["course"]["sequenceAndFamily"])
+        # print(response.json()[courses][i]["course"]["title"])
+        # #     startDate, 
+        # print(response.json()[courses][i]["course"]["startDate"])
+        # #     track, 
+        # print(response.json()[courses][i]["course"]["track"])
+        # #     price, 
+        # print(response.json()[courses][i]["course"]["price"])
+        # #     sequenceAndFamily.
+        # print(response.json()[courses][i]["course"]["sequenceAndFamily"])
 
+def calendar_dict(URL):
+    response = requests.get(URL)
+    print("ordered keys: ")
+    ordered_data = json.loads(response.json()["courses"], object_pairs_hook=OrderedDict)
+    print("Type: ", type(OrderedData))
+    print(OrderedData)
          
 
 
@@ -36,4 +62,5 @@ def cf_calendar(URL):
 if __name__ == "__main__":
     URL = "https://s3-us-west-2.amazonaws.com/static.codefellows.org/courses/schedule.json"
     # data_harvest(URL)
-    cf_calendar(URL)
+    # cf_calendar(URL)
+    calendar_dict(URL)
