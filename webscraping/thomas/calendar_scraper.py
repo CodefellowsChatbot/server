ef1efa3bf898fc61abc4f355c6e5df51e85f1f28
vorSherer <tcpma2011@gmail.com>
2020-08-17 16:19:17 -0700

    Working code to filter specific course info from scraped source.

--- webscraping/thomas/calendar_scraper.py
+++ webscraping/thomas/calendar_scraper.py
@@ -1,17 +1,6 @@
-# import requests, os, sys
-
-# currentdir = os.path.dirname(os.path.realpath(__file__))
-# parentdir = os.path.dirname(currentdir)
-# sys.path.append(parentdir)
-
-# from resources.links import Links
-
-# links = Links()
-
-# URL = links.course_calendar_json
-# ***********************************************
-
 import requests
+import pprint   # Pretty Print
+
 
 URL = "https://s3-us-west-2.amazonaws.com/static.codefellows.org/courses/schedule.json"
 
@@ -26,53 +15,62 @@ def calendar_data(URL):
 
     for course in cf_courses:
         cf_normalized = {
-        course["course"]["title"] : {
-            'code' : course["course"]["code"],
-            'start date' : course["course"]["startDate"],
-            'track' : course["course"]["track"],
-            'handle' : course["course"]["sequenceAndFamily"]
-        }}
+        'code' : course["course"]["code"],
+        'title' : course["course"]["title"],
+        'start date' : course["course"]["startDate"],
+        'track' : course["course"]["track"],
+        'handle' : course["course"]["sequenceAndFamily"]
+        }
 
         calendar.append(cf_normalized)
 
-# sort the calendar by start date:
-
-    # sorted_calendar = sorted(calendar, key="start date")
-
-    print(calendar)     # REMOVE THIS ****************************
-    # return calendar
+    # sort the calendar by start date:
+    sorted_calendar = sorted(calendar, key=lambda course: course["start date"])
 
 
-def course_calendar_by_track(handle):
-    pass
-
-    # course_by_track = ""
-    # for offering in calendar:
-    #     if handle in course['handle'] and track in course['track']:
-
-    # calendar_data()
+    # pprint.pp(sorted_calendar)     # REMOVE THIS ****************************
+    return sorted_calendar
 
 
-# def course_calendar_by_course(handle):
-#     pass
-#     course_cat = {}
+def course_calendar_by_track(calendar, track):
 
-#     source_list = calendar_data(URL)
-#     # try:
-#     for offering in source_list:
-#         for item in offering:
-#             try:
-#                 course_cat[offering[item]["handle"]]
-#             except KeyError:
-#                 course_cat[offering[item]["handle"]] = []
-#             course_cat[offering[item]["handle"]].append(offering)
-#     # except:
-#     #     print("I don't think that means what you think it means.")
+    day_track_course = []
+    night_track_course = []
 
-#     print(course_cat["code-101"])
+    for course_track in calendar:
+        if course_track["track"] == "day":
+            day_track_course.append(course_track)
+        elif course_track["track"] == "night":
+            night_track_course.append(course_track)
+
+    if track == "day":
+        return day_track_course
+    elif track == "night":
+        return night_track_course
 
 
+def course_calendar_by_course(calendar, handle):
 
+    course_cat = []
+
+    for course_code in calendar:
+        try:
+            if course_code["handle"] == handle:
+                course_cat.append(course_code)
+        except KeyError:
+            course_cat[course_code["handle"]] = []
+    
+    return course_cat[0]
+        
+
+def course_filters(calendar, course_family=None, track=None):
+    if course_family:
+        result = course_calendar_by_course(calendar, course_family)
+    if track:
+        track_info = course_calendar_by_track(calendar, track)
+        result = course_calendar_by_course(track_info, course_family)
+
+    return result
 
 
 
@@ -80,5 +78,11 @@ def course_calendar_by_track(handle):
 
 if __name__ == "__main__":
     URL = "https://s3-us-west-2.amazonaws.com/static.codefellows.org/courses/schedule.json"
-    calendar_data(URL)
-    # course_calendar_by_course("code-201")
+    sorted_calendar = calendar_data(URL)
+    # pprint.pp(sorted_calendar)
+    # course_filters(sorted_calendar, "code-201", "night")
+    # sample = course_calendar_by_course(sorted_calendar, "code-201")
+    sample = course_filters(sorted_calendar, "code-301", "day")
+    # sample = course_filters(sorted_calendar, "code-201")
+
+    print(sample)
